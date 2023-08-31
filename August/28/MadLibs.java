a618d2ed96b955dc4a2d4b8879195de7ad1bd42d
Casey Barajas <laughablelosers69@gmail.com>
2023-08-31 16:11:46 -0500

    Update MadLibs.java

--- August/28/MadLibs.java
+++ August/28/MadLibs.java
@@ -6,7 +6,6 @@
 
 import java.util.Scanner;
 
-
 public class MadLibs {
     
     public static void main(String[] args) {
@@ -21,15 +20,14 @@ public class MadLibs {
         int numberOfCheeses;
         double clownShoeSize;
         boolean isScaredOfClowns;
-        String name;
-        String typeOfCheese;
-
+        String name, typeOfCheese, clownName, adjective, place, action, animal, hobby;
+        
         /* Data Collection */
-        // Name
+        // 1. Name
         System.out.println("Hey, what's your name?");
         name = scan.nextLine();
         
-        // Number of Cheeses with Data Insurance
+        // 2. Number of Cheeses with Data Insurance
         do {
             System.out.println("How many types of cheese do you own?");
             try {
@@ -40,13 +38,37 @@ public class MadLibs {
             }
         } while (true);
 
-        // Favorite Type of Cheese
+        // 3. Favorite Type of Cheese
         System.out.println("What's your favorite type of cheese?");
         typeOfCheese = scan.nextLine();
         
-        // Clown Shoe Size with Data Insurance
+        // 4. Clown Name
+        System.out.println("What's the name of the clown in your story?");
+        clownName = scan.nextLine();
+        
+        // 5. Adjective
+        System.out.println("Give me an adjective:");
+        adjective = scan.nextLine();
+
+        // 6. Place
+        System.out.println("Name a place:");
+        place = scan.nextLine();
+
+        // 7. Action
+        System.out.println("Name an action:");
+        action = scan.nextLine();
+
+        // 8. Animal
+        System.out.println("Name an animal:");
+        animal = scan.nextLine();
+
+        // 9. Hobby
+        System.out.println("Name a hobby:");
+        hobby = scan.nextLine();
+        
+        // 10. Clown Shoe Size with Data Insurance
         do {
-            System.out.println("What's your clown shoe size?");
+            System.out.println("What's the clown's shoe size?");
             try {
                 clownShoeSize = Double.parseDouble(scan.nextLine());
                 break;
@@ -55,22 +77,22 @@ public class MadLibs {
             }
         } while (true);
 
-        // Fear of Clowns
+        // 11. Fear of Clowns
         System.out.println("Are you scared of clowns? (true/false)");
         isScaredOfClowns = Boolean.parseBoolean(scan.nextLine());
 
         /* Story Time */
-        // Let's see what we've got
+        // Outputting the Mad Lib story
         System.out.println("\nAlright, here's your Mad Lib story:");
         System.out.println(name + " was a connoisseur of cheese, owning exactly " + numberOfCheeses + " types.");
-        System.out.println("One day, while enjoying some " + typeOfCheese + ", a clown with a shoe size of " + clownShoeSize + " burst into the room.");
-        System.out.println("The clown handed " + name + " a balloon and said, 'I have a message for you.'");
-        System.out.println("Feeling " + (isScaredOfClowns ? "terrified" : "intrigued") + ", " + name + " popped the balloon.");
-        System.out.println("Inside the balloon was another piece of " + typeOfCheese + "!");
+        System.out.println("One day in " + place + ", while enjoying some " + typeOfCheese + ", a clown named " + clownName + " with a shoe size of " + clownShoeSize + " burst into the room.");
+        System.out.println("The " + adjective + " clown handed " + name + " a " + animal + " shaped balloon and said, 'I have a message for you.'");
+        System.out.println("Feeling " + (isScaredOfClowns ? "terrified" : "intrigued") + ", " + name + " decided to " + action + " the balloon.");
+        System.out.println("Inside the balloon was another piece of " + typeOfCheese + " and a note about " + hobby + "!");
         System.out.println("'This is a cheesy miracle!' exclaimed " + name + ".");
-        System.out.println("The clown's oversized shoes suddenly deflated, shrinking down to regular size.");
+        System.out.println("The clown's oversized shoes suddenly deflated, shrinking down to " + clownShoeSize + ".");
         System.out.println("It turns out the clown was a cheese genie, capable of granting " + numberOfCheeses + " cheese-related wishes.");
         System.out.println("So, " + name + " wished for a lifetime supply of " + typeOfCheese + ", a cheese moon, and for all clowns to never be scary again.");
-        System.out.println("And they all lived cheesily ever after.");
+        System.out.println("And they all lived cheesily ever after in " + place + ".");
     }
 }
